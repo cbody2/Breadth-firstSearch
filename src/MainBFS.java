13f5ee314cc8f97f0c7b717d1474d2aeb59618ae
Carrington Body <carringtonbody@gmail.com>
2022-10-24 14:34:39 -0500

    Initial commit

---
+++ src/MainBFS.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MainBFS {
+
+    private Queue<String> q;
+
+    public MainBFS() {
+        q = new LinkedList<String>();
+    }
+
+    public static void main(String[] args) {
+        String filename = args[0];
+
+    }
+}
